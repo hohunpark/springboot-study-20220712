d74d93314d7566cc042cd8d87f7b5a375f3a7afd
DESKTOP-2PESCFV\USER <pophh@naver.com>
2022-07-12 20:52:42 +0900

    최초

---
+++ src/main/java/com/springboot/studyhohun/StudyHohunApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyhohun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHohunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHohunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studyhohun/StudyHohunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studyhohun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHohunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
